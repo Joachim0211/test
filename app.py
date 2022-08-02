08ac0d34b0a4b8d0739a9030558b9539ce90cd27
Joachim <joachim0509@gmail.com>
2022-08-02 20:45:54 +0200

    Added required files for heroku

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -6,7 +7,10 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
-if __name__ == "__main__":
-    app.run(debug=True)
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
 
-app.run()
+if __name__ == "__main__":
+    app.run(debug=True, port=port)
+
+app.run(debug=True, port=port)
